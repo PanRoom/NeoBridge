c890bedfbe6e68a406b33096aa83fc48aca72429
e235733@ie.u-ryukyu.ac.jp <hamzayunta17@icloud.com>
2025-09-17 13:21:10 +0900

    Initial Commit

---
+++ NeoBridge/NeoBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoBridgeApp.swift
+//  NeoBridge
+//
+//  Created by 比嘉拓貴 on 2025/09/17.
+//
+
+import SwiftUI
+
+@main
+struct NeoBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
